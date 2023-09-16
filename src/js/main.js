ac99c7f4751961a22fd9016ee9643fd248f1e0ca
Louis Pallett <louis.pallett@outlook.com>
2023-09-16 20:52:08 +0100

    Basic functions calling weather info (to console). Basic elements in html

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,37 @@
 import '../scss/styles.scss';
 import axios from 'axios';
 
-console.log("Hello, world!");
+const location = "Istanbul";
+const image = document.querySelector("img");
+
+const fetchWeather = (() => {
+    const showWeather = (async(userLocation) => {
+        try {
+            const weatherData = await axios.get("http://api.weatherapi.com/v1/current.json?key=d2404bd088b24d72a1164040231309&q=" + userLocation);
+            
+            const location = weatherData.data.location;
+            const data = weatherData.data;
+
+            const country = location.country;
+            let nameLocation;
+            if(country == "United States of America") {
+                nameLocation = `${location.name}, ${location.region}`;
+            } else {
+                nameLocation = `${location.name}, ${country}`;
+            }
+
+            console.log(data);
+            const current = data.current;
+            const tempC = current.temp_c;
+
+            console.log(`Location: ${nameLocation}`);
+            console.log(tempC);
+        } catch {
+            console.error("No data found!");
+        }
+    });
+
+    return { showWeather };
+})();
+
+fetchWeather.showWeather(location);
