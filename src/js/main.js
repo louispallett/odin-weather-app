b0432ea8c88f583b41e1ddab74836c07c479bcb6
Louis Pallett <louis.pallett@outlook.com>
2023-09-20 11:38:33 +0100

    Refactored main.js: Content now immediately loads to London via document event listener. fillData() added to seperate functions out.

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,6 @@ import '../scss/styles.scss';
 // import axios from 'axios';
 import { showTop, showBottom } from "./app-info"
 
-const appData = {
-    city: "",
-    country: "",
-    description: "",
-    tempCel: 0,
-    feelsLike: 0,
-    windDir: 0,
-    windKph: 0,
-    humitity: 0,
-};
-
 const fetchData = async (userInput) => {
     try {
         const response = await fetch("http://api.weatherapi.com/v1/current.json?key=d2404bd088b24d72a1164040231309&q=" + userInput, { mode: 'cors' });
@@ -24,7 +13,7 @@ const fetchData = async (userInput) => {
     }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
+const submitNewLocation = (() => {
     const submitBtn = document.querySelector(".search-btn");
     const inputElement = document.querySelector("input");
 
@@ -34,28 +23,41 @@ document.addEventListener("DOMContentLoaded", () => {
         const userInput = inputElement.value;
         const weatherData = await fetchData(userInput);
 
+        let country;
+
         if (weatherData) {
             const locationData = weatherData.location;
-            let country;
             if (locationData.country == "United States of America") {
                 country = locationData.region;
             } else {
                 country = locationData.country;
             }
 
-            // Update the appData object with fetched data
-            appData.city = weatherData.location.name;
-            appData.country = country;
-            appData.description = weatherData.current.condition.text;
-            appData.tempCel = weatherData.current.temp_c;
-            appData.feelsLike = weatherData.current.feelslike_c;
-            appData.windDir = weatherData.current.wind_degree;
-            appData.windKph = weatherData.current.wind_kph;
-            appData.humitity = weatherData.current.humidity;
         }
-
-        console.log(weatherData);
-        showTop(appData.city, appData.country, appData.tempCel, appData.description);
-        showBottom(appData.feelsLike, appData.humitity, appData.windKph, appData.windDir);
+        
+        fillData(weatherData);
     });
+})();
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const userInput = "London";
+    const weatherData = await fetchData(userInput);
+
+    fillData(weatherData);
+});
+
+const fillData = ((weatherData) => {
+    const appData = {
+        city: weatherData.location.name,
+        country: weatherData.location.country,
+        description: weatherData.current.condition.text,
+        tempCel: weatherData.current.temp_c,
+        feelsLike: weatherData.current.feelslike_c,
+        windDir: weatherData.current.wind_degree,
+        windKph: weatherData.current.wind_kph,
+        humitity: weatherData.current.humidity,
+    }
+
+    showTop(appData.city, appData.country, appData.tempCel, appData.description);
+    showBottom(appData.feelsLike, appData.humitity, appData.windKph, appData.windDir);
 });
