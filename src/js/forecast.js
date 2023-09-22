5356b22524e1c1f41d128f311da3b681a7927106
louispallett <louis.pallett@outlook.com>
2023-09-22 15:38:01 +0100

    Now pulls through forcast info via showForcast()

--- src/js/forecast.js
+++ src/js/forecast.js
@@ -5,11 +5,9 @@ const showForecast = ((forecastArray) => {
     for(let i = 1; i < forecastArray.length; i++) {
         const dayElement = document.querySelector(".day" + i);
         removesChildren(dayElement);
-        dayElement.appendChild(showDate(forecastArray[i].date))
-        // console.log(forecastArray[i].date);
-        // console.log(forecastArray[i].day.maxtemp_c);
-        // console.log(forecastArray[i].day.mintemp_c);
-        // console.log(forecastArray[i].day.condition.text);
+        dayElement.appendChild(showDate(forecastArray[i].date));
+        dayElement.appendChild(showTemp(forecastArray[i].day.avgtemp_c));
+        dayElement.appendChild(showText(forecastArray[i].day.condition.text))
     }
 });
 
@@ -17,7 +15,19 @@ const showDate = ((date) => {
     const dateInfo = document.createElement("div");
     dateInfo.textContent = date;
     return dateInfo;
-})
+});
+
+const showTemp = ((temp) => {
+    const tempInfo = document.createElement("div");
+    tempInfo.textContent = temp;
+    return tempInfo;
+});
+
+const showText = ((text) => {
+    const textInfo = document.createElement("div");
+    textInfo.textContent = text;
+    return textInfo;
+});
 
 const removesChildren = ((parent) => {
     while(parent.lastChild) {
