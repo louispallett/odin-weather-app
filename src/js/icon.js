9d62167e7970bd59195019bb945bbd0aad14843b
louispallett <louis.pallett@outlook.com>
2023-09-27 19:24:19 +0100

    Comments on showIcon()

--- src/js/icon.js
+++ src/js/icon.js
@@ -1,9 +1,25 @@
-// import rainyIcon from "../assets/rainy.svg";
+import rainyIcon from "../assets/rainy.svg";
 export { showIcon };
 
-const showIcon = (() => {
-    const wrapper = document.querySelector(".wrapper");
-    const icon = document.createElement("img");
-    icon.src = rainyIcon;
-    wrapper.appendChild(icon);
-})
+/* Instructions:
+    It would be ideal if this could be used for both the forecast icons AND the current 
+    weather icon. 
+    
+    To so this, probably best to RETURN the icon in question, so that the <img> element
+    can be SET to it?*/
+
+// const showIcon = (() => {
+//     const wrapper = document.querySelector(".wrapper");
+//     const icon = document.createElement("img");
+//     icon.src = rainyIcon;
+//     wrapper.appendChild(icon);
+// })
+
+// WARNING: below function is temporary, just outlining how it SHOULD work, but
+// I have no internet :(
+const showIcon = ((weatherCode) => {
+    //if statement here
+    if(weatherCode >= 1100 && weatherCode <= 1200) {
+        return rainyIcon;
+    } //else if and so on
+});
